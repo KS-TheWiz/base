f78c7efdff03602c3e14a3f51d236460476837d0
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-02-28 17:15:12 +0100

    Base: Custom scrolling values 1/2
    ported/modified from https://gerrit.omnirom.org/#/c/4251/
    originally written by Yanuar Harry <ai.the.smarties.physics@gmail.com>
    
    Change-Id: I4002de36364e9556891ddb82201f5153db68e6e6
    Signed-off-by: KS-TheWiz <ksthewiz@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4010,6 +4010,35 @@ public final class Settings {
          public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
+         * @hide
+         */
+        public static final String ANIMATION_CONTROLS_NO_SCROLL = "animation_controls_no_scroll";
+
+        /**
+         * Determine custom scroll friction.
+         * @hide
+         */
+        public static final String CUSTOM_SCROLL_FRICTION = "custom_scroll_friction";
+
+        /**
+         * Determine custom fling velocity.
+         * @hide
+         */
+        public static final String CUSTOM_FLING_VELOCITY = "custom_fling_velocity";
+
+        /**
+         * Determine custom overscroll distance.
+         * @hide
+         */
+        public static final String CUSTOM_OVERSCROLL_DISTANCE = "custom_overscroll_distance";
+
+        /**
+         * Determine custom overfling distance.
+         * @hide
+         */
+        public static final String CUSTOM_OVERFLING_DISTANCE = "custom_overfling_distance";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
