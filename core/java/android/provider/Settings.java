5a6b5e3a87967db6ad69e342b3c448171ed48805
ghbhaha <ghbhaha@gmail.com>
2015-08-10 20:15:27 -0400

    Add three-fingers-swipe to screenshot [1/2]
    The feature is ported from Oppo ColorOS.
    With the options on, users can swipe down
    on the screen to have a screenshot.
    
    -Sources: SudaMod
     https://github.com/SudaMod
    
    -Original smali codes: wuxianlin
    
    Change-Id: I2cb0a9a3e31647dffc4202ce4d5bfe3e46983317
    Signed-off-by: KS-TheWiz <ksthewiz@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4225,6 +4225,13 @@ public final class Settings {
         public static final String ANIM_TILE_INTERPOLATOR = "anim_tile_interpolator";
 
         /**
+         * Three Finger Gesture from Oppo
+         *
+         * @hide
+         */
+        public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
